4b8cd50fca4ef6b007e59493b4ca507481c0fdc1
Kevin Johne <8806052+kevin-johne@users.noreply.github.com>
2022-05-20 18:30:44 +0100

    optimize when to calculate

--- src/modules/teaser.jsx
+++ src/modules/teaser.jsx
@@ -1,9 +1,10 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { InlineLink } from "../patterns/link";
 import { colorGray75, font } from "../setting/theme";
 import Tommy from "../images/icons/tommy.svg";
 import { useAnimationFrame } from "framer-motion";
+import { debounce } from "lodash";
 
 const Wrapper = styled.div`
   position: relative;
@@ -53,20 +54,39 @@ export const Teaser = ({ title, subtitle }) => {
   let ctx;
   const [radius, setRadius] = useState(0);
   const diagonal = 2.5;
+  const targetFrames = 40;
+  const oneOffASecond = 1/targetFrames * 1000;
+  let oldTime = 0;
+  const speed = 0.5;
 
-  useEffect(() => {
+  const windowResponsive = () => {
     const box = wrapperRef.current.getBoundingClientRect();
     const radius = Math.min(box.width, box.height) / 2 / diagonal;
-    ctx = canvasRef.current.getContext('2d');
     canvasRef.current.height = box.height;
     canvasRef.current.width = box.width;
     setRadius(radius);
-  }, [wrapperRef, canvasRef, setRadius]);
+  };
 
-  useAnimationFrame((t) => {
+  useEffect(() => {
+    ctx = canvasRef.current.getContext('2d');
+    windowResponsive();
+  },[]);
 
-    
-    const x = Math.sin(t / 1000) * radius * diagonal;
+  useEffect(() => {
+    const debounced = debounce(windowResponsive, 2000);
+    window.addEventListener('resize', debounced);
+
+    return () => {
+      window.removeEventListener('resize', debounced);
+    };
+  }, []);
+
+  useAnimationFrame((time) => {
+    if(time - oldTime <= oneOffASecond) {
+      return;
+    }
+
+    const x = Math.sin(time / 1000 * speed) * radius * diagonal;
     if (x <= oldx) {
       switchCircle = -1;
     }
@@ -75,17 +95,18 @@ export const Teaser = ({ title, subtitle }) => {
       switchCircle = 1;
     }
     const y = switchCircle * Math.sqrt(Math.pow(radius, 2) - Math.pow(x / diagonal, 2)) - x / diagonal;
-    // console.log(x);
 
-    logoGlobeRef.current.style.transform = `translateY(${y}px) translateX(${x}px)`;
+    const hypotenuses = x / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) + 2;
+    logoGlobeRef.current.style.transform = `translateY(${y}px) translateX(${x}px) scale(${hypotenuses})`;
 
     if(!ctx) {
       ctx = canvasRef.current.getContext('2d');
     }
-    ctx.fillRect(x+canvasRef.current.width/2,y + canvasRef.current.height /2 ,1,1);
+    ctx.fillRect(x+canvasRef.current.width/2,y + canvasRef.current.height /2 ,hypotenuses,hypotenuses);
 
     oldx = x;
     oldy = y;
+    oldTime = time;
   });
 
   return (
@@ -99,9 +120,6 @@ export const Teaser = ({ title, subtitle }) => {
         <Circle ref={logoGlobeRef}><img src={Tommy}/></Circle>
       </div>
       <Canvas id="canvas" ref={canvasRef}></Canvas>
-
-      {/* sqrt(25-(x*0.5)^2)+x/4 */}
-      {/* -sqrt(25-(x*0.5)^2)+x/4 */}
     </Wrapper>
   );
 };
